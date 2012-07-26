8579b9f0b991449860ea77a817f32a5474154a0e
Michael.Kang <blackfin.kang@gmail.com>
2012-07-26 11:32:08 +0800

    Add memory related changes from S2E
    Add memory related changes from S2E
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- softmmu_defs.h
+++ softmmu_defs.h
@@ -18,5 +18,29 @@ uint32_t REGPARM __ldl_cmmu(target_ulong addr, int mmu_idx);
 void REGPARM __stl_cmmu(target_ulong addr, uint32_t val, int mmu_idx);
 uint64_t REGPARM __ldq_cmmu(target_ulong addr, int mmu_idx);
 void REGPARM __stq_cmmu(target_ulong addr, uint64_t val, int mmu_idx);
+#ifdef CONFIG_S2E
+
+uint8_t REGPARM io_readb_mmu_s2e_trace(target_phys_addr_t physaddr, target_ulong addr, void *retaddr);
+void REGPARM io_writeb_mmu_s2e_trace(target_phys_addr_t physaddr, uint8_t val, target_ulong addr, void *retaddr);
+uint16_t REGPARM io_readw_mmu_s2e_trace(target_phys_addr_t physaddr, target_ulong addr, void *retaddr);
+void REGPARM io_writew_mmu_s2e_trace(target_phys_addr_t physaddr, uint16_t val, target_ulong addr, void *retaddr);
+uint32_t REGPARM io_readl_mmu_s2e_trace(target_phys_addr_t physaddr, target_ulong addr, void *retaddr);
+void REGPARM io_writel_mmu_s2e_trace(target_phys_addr_t physaddr, uint32_t val, target_ulong addr, void *retaddr);
+uint64_t REGPARM io_readq_mmu_s2e_trace(target_phys_addr_t physaddr, target_ulong addr, void *retaddr);
+void REGPARM io_writeq_mmu_s2e_trace(target_phys_addr_t physaddr, uint64_t val, target_ulong addr, void *retaddr);
+
+uintptr_t s2e_notdirty_mem_write(target_phys_addr_t ram_addr);
+int s2e_ismemfunc(void *f);
+
+uint8_t REGPARM __ldb_mmu_s2e_trace(target_ulong addr, int mmu_idx);
+void REGPARM __stb_mmu_s2e_trace(target_ulong addr, uint8_t val, int mmu_idx);
+uint16_t REGPARM __ldw_mmu_s2e_trace(target_ulong addr, int mmu_idx);
+void REGPARM __stw_mmu_s2e_trace(target_ulong addr, uint16_t val, int mmu_idx);
+uint32_t REGPARM __ldl_mmu_s2e_trace(target_ulong addr, int mmu_idx);
+void REGPARM __stl_mmu_s2e_trace(target_ulong addr, uint32_t val, int mmu_idx);
+uint64_t REGPARM __ldq_mmu_s2e_trace(target_ulong addr, int mmu_idx);
+void REGPARM __stq_mmu_s2e_trace(target_ulong addr, uint64_t val, int mmu_idx);
+
+#endif
 
 #endif
--- softmmu_header.h
+++ softmmu_header.h
@@ -77,8 +77,40 @@
 #define ADDR_READ addr_read
 #endif
 
-/* generic load/store macros */
+#ifdef CONFIG_S2E
+#include <s2e/s2e_config.h>
 
+#ifdef S2E_LLVM_LIB
+#define S2E_TRACE_MEMORY(vaddr, haddr, value, isWrite, isIO) \
+    tcg_llvm_trace_memory_access(vaddr, haddr, \
+                                 value, 8*sizeof(value), isWrite, isIO);
+#define S2E_FORK_AND_CONCRETIZE(val, max) \
+    tcg_llvm_fork_and_concretize(val, 0, max)
+#else // S2E_LLVM_LIB
+#define S2E_TRACE_MEMORY(vaddr, haddr, value, isWrite, isIO) \
+    s2e_trace_memory_access(vaddr, haddr, \
+                            (uint8_t*) &value, sizeof(value), isWrite, isIO);
+#define S2E_FORK_AND_CONCRETIZE(val, max) (val)
+#endif // S2E_LLVM_LIB
+
+#define S2E_FORK_AND_CONCRETIZE_ADDR(val, max) \
+    (g_s2e_fork_on_symbolic_address ? S2E_FORK_AND_CONCRETIZE(val, max) : val)
+
+#define S2E_RAM_OBJECT_DIFF (TARGET_PAGE_BITS - S2E_RAM_OBJECT_BITS)
+
+#else // CONFIG_S2E
+
+#define S2E_TRACE_MEMORY(...)
+#define S2E_FORK_AND_CONCRETIZE(val, max) (val)
+#define S2E_FORK_AND_CONCRETIZE_ADDR(val, max) (val)
+
+#define S2E_RAM_OBJECT_BITS TARGET_PAGE_BITS
+#define S2E_RAM_OBJECT_DIFF 0
+
+#endif // CONFIG_S2E
+
+/* generic load/store macros */
+#ifndef CONFIG_S2E
 static inline RES_TYPE glue(glue(ld, USUFFIX), MEMSUFFIX)(target_ulong ptr)
 {
     int page_index;
@@ -144,6 +176,114 @@ static inline void glue(glue(st, SUFFIX), MEMSUFFIX)(target_ulong ptr, RES_TYPE 
         glue(glue(st, SUFFIX), _raw)((uint8_t *)physaddr, v);
     }
 }
+#endif
+/* generic load/store macros */
+
+static inline RES_TYPE glue(glue(ld, USUFFIX), MEMSUFFIX)(target_ulong ptr)
+{
+    int object_index, page_index;
+    RES_TYPE res;
+    target_ulong addr;
+    uintptr_t physaddr;
+    int mmu_idx;
+
+    addr = S2E_FORK_AND_CONCRETIZE_ADDR(ptr, ADDR_MAX);
+    object_index = S2E_FORK_AND_CONCRETIZE(addr >> S2E_RAM_OBJECT_BITS,
+                                           ADDR_MAX >> S2E_RAM_OBJECT_BITS);
+    page_index = (object_index >> S2E_RAM_OBJECT_DIFF) & (CPU_TLB_SIZE - 1);
+
+    mmu_idx = CPU_MMU_INDEX;
+    if (unlikely(env->tlb_table[mmu_idx][page_index].ADDR_READ !=
+                 (addr & (TARGET_PAGE_MASK | (DATA_SIZE - 1))))) {
+        res = glue(glue(__ld, SUFFIX), MMUSUFFIX)(addr, mmu_idx);
+    } else {
+        //When we get here, the address is aligned with the size of the access,
+        //which by definition means that it will fall inside the small page, without overflowing.
+        physaddr = addr + env->tlb_table[mmu_idx][page_index].addend;
+
+#if defined(CONFIG_S2E) && defined(S2E_ENABLE_S2E_TLB) && !defined(S2E_LLVM_LIB)
+        S2ETLBEntry *e = &env->s2e_tlb_table[mmu_idx][object_index & (CPU_S2E_TLB_SIZE-1)];
+        if(likely(_s2e_check_concrete(e->objectState, addr & ~S2E_RAM_OBJECT_MASK, DATA_SIZE)))
+            res = glue(glue(ld, USUFFIX), _p)((uint8_t*)(addr + (e->addend&~1)));
+        else
+#endif
+            res = glue(glue(ld, USUFFIX), _raw)((uint8_t *)physaddr);
+
+        //XXX: Fix this to be on the dataflow
+        //res = S2E_TRACE_MEMORY(addr, physaddr, res, 0, 0);
+        S2E_TRACE_MEMORY(addr, physaddr, res, 0, 0);
+    }
+    return res;
+}
+
+#if DATA_SIZE <= 2
+static inline int glue(glue(lds, SUFFIX), MEMSUFFIX)(target_ulong ptr)
+{
+    int res, object_index, page_index;
+    target_ulong addr;
+    uintptr_t physaddr;
+    int mmu_idx;
+
+    addr = S2E_FORK_AND_CONCRETIZE_ADDR(ptr, ADDR_MAX);
+    object_index = S2E_FORK_AND_CONCRETIZE(addr >> S2E_RAM_OBJECT_BITS,
+                                           ADDR_MAX >> S2E_RAM_OBJECT_BITS);
+    page_index = (object_index >> S2E_RAM_OBJECT_DIFF) & (CPU_TLB_SIZE - 1);
+
+    mmu_idx = CPU_MMU_INDEX;
+    if (unlikely(env->tlb_table[mmu_idx][page_index].ADDR_READ !=
+                 (addr & (TARGET_PAGE_MASK | (DATA_SIZE - 1))))) {
+        res = (DATA_STYPE)glue(glue(__ld, SUFFIX), MMUSUFFIX)(addr, mmu_idx);
+    } else {
+        physaddr = addr + env->tlb_table[mmu_idx][page_index].addend;
+
+#if defined(CONFIG_S2E) && defined(S2E_ENABLE_S2E_TLB) && !defined(S2E_LLVM_LIB)
+        S2ETLBEntry *e = &env->s2e_tlb_table[mmu_idx][object_index & (CPU_S2E_TLB_SIZE-1)];
+        if(likely(_s2e_check_concrete(e->objectState, addr & ~S2E_RAM_OBJECT_MASK, DATA_SIZE)))
+            res = glue(glue(lds, SUFFIX), _p)((uint8_t*)(addr + (e->addend&~1)));
+        else
+#endif
+            res = glue(glue(lds, SUFFIX), _raw)((uint8_t *)physaddr);
+
+        S2E_TRACE_MEMORY(addr, physaddr, res, 0, 0);
+    }
+    return res;
+}
+#endif
+
+#if ACCESS_TYPE != (NB_MMU_MODES + 1)
+
+/* generic store macro */
+
+static inline void glue(glue(st, SUFFIX), MEMSUFFIX)(target_ulong ptr, RES_TYPE v)
+{
+    int object_index, page_index;
+    target_ulong addr;
+    uintptr_t physaddr;
+    int mmu_idx;
+
+    addr = S2E_FORK_AND_CONCRETIZE_ADDR(ptr, ADDR_MAX);
+    object_index = S2E_FORK_AND_CONCRETIZE(addr >> S2E_RAM_OBJECT_BITS,
+                                           ADDR_MAX >> S2E_RAM_OBJECT_BITS);
+    page_index = (object_index >> S2E_RAM_OBJECT_DIFF) & (CPU_TLB_SIZE - 1);
+
+    mmu_idx = CPU_MMU_INDEX;
+    if (unlikely(env->tlb_table[mmu_idx][page_index].addr_write !=
+                 (addr & (TARGET_PAGE_MASK | (DATA_SIZE - 1))))) {
+        glue(glue(__st, SUFFIX), MMUSUFFIX)(addr, v, mmu_idx);
+    } else {
+        physaddr = addr + env->tlb_table[mmu_idx][page_index].addend;
+
+#if defined(CONFIG_S2E) && defined(S2E_ENABLE_S2E_TLB) && !defined(S2E_LLVM_LIB)
+        S2ETLBEntry *e = &env->s2e_tlb_table[mmu_idx][object_index & (CPU_S2E_TLB_SIZE-1)];
+        if(likely((e->addend & 1) && _s2e_check_concrete(e->objectState, addr & ~S2E_RAM_OBJECT_MASK, DATA_SIZE)))
+            glue(glue(st, SUFFIX), _p)((uint8_t*)(addr + (e->addend&~1)), v);
+        else
+#endif
+            glue(glue(st, SUFFIX), _raw)((uint8_t *)physaddr, v);
+
+        S2E_TRACE_MEMORY(addr, physaddr, v, 1, 0);
+    }
+}
 
 #endif /* ACCESS_TYPE != (NB_MMU_MODES + 1) */
 
@@ -194,6 +334,13 @@ static inline void glue(stfl, MEMSUFFIX)(target_ulong ptr, float32 v)
 #endif /* DATA_SIZE == 4 */
 
 #endif /* ACCESS_TYPE != (NB_MMU_MODES + 1) */
+#ifndef CONFIG_S2E
+#undef S2E_RAM_OBJECT_BITS
+#endif
+#undef S2E_RAM_OBJECT_DIFF
+#undef S2E_FORK_AND_CONCRETIZE
+#undef S2E_TRACE_MEMORY
+#undef ADDR_MAX
 
 #undef RES_TYPE
 #undef DATA_TYPE
