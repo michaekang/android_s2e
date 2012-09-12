73e95a06dbf20b7ccb12520c77667b2590aa3db0
Michael.Kang <blackfin.kang@gmail.com>
2012-09-12 15:12:18 +0800

    Add config file for s2e t2 testcase
    Add config file for s2e t2 testcase
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

---
+++ tests/config.lua
@@ -0,0 +1,11 @@
+s2e = {
+  kleeArgs = {
+    -- Run each state for at least 1 second before
+    -- switching to the other:
+    "--use-batching-search=true", "--batch-time=1.0"
+  }
+}
+plugins = {
+  -- Enable a plugin that handles S2E custom opcode
+  "BaseInstructions"
+}
